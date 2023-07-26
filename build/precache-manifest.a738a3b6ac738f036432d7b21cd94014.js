4d5632f634e2948dae96bcfbc15be730d6b32940
Felipe Moreira da Cruz <piradous@gmail.com>
2023-07-26 09:54:58 -0300

    adicionando arquivos de build do projeto

---
+++ build/precache-manifest.a738a3b6ac738f036432d7b21cd94014.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb6f09cc7b33965dd1060c7ea7a6f99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7794e92a548047cfdc24",
+    "url": "/static/css/main.e28f0003.chunk.css"
+  },
+  {
+    "revision": "dd8fec9bb0e130704a81",
+    "url": "/static/js/2.ed93f78b.chunk.js"
+  },
+  {
+    "revision": "0b67a4c8b98bc3c3d0fbf02adaaea70e",
+    "url": "/static/js/2.ed93f78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7794e92a548047cfdc24",
+    "url": "/static/js/main.acd583d7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.acd583d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01da206b4def506d6263",
+    "url": "/static/js/runtime-main.af69943c.js"
+  },
+  {
+    "revision": "ca41588a6962ab35495b1d95781c210f",
+    "url": "/static/media/ampulheta.ca41588a.png"
+  },
+  {
+    "revision": "d8fbb0ead2d323c7225d1c229516af71",
+    "url": "/static/media/check.d8fbb0ea.png"
+  },
+  {
+    "revision": "5a5541973d74c83364be89fdd3eeac58",
+    "url": "/static/media/interrogation.5a554197.png"
+  },
+  {
+    "revision": "b523ea0d6874751c4e73cc69e32473e0",
+    "url": "/static/media/wrong.b523ea0d.png"
+  }
+]);
